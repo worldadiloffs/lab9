a1a24ca952779a84fc3966e7d8a50dfaa39ec61a
Abdumajid <abdumajidadilov1@gmail.com>
2025-03-13 11:56:04 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+int multiply(int a, int b) {
+    if (b == 0) return 0;
+    if (b > 0) return a + multiply(a, b - 1);
+    return -multiply(a, -b);
+}
+
+void p1() {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Product: " << multiply(num1, num2) << endl;
+}
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+void p2() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << factorial(num) << endl;
+}
+
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void p3() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+}
+
+int sum(int n) {
+    if (n == 0) return 0;
+    return n + sum(n - 1);
+}
+
+void p4() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << sum(n) << endl;
+}
+
+void descendingOrder(int n) {
+    if (n == 0) return;
+    cout << n << " ";
+    descendingOrder(n - 1);
+}
+
+void p5() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    descendingOrder(n);
+    cout << endl;
+}
+
+int countDigits(int n) {
+    if (n == 0) return 0; // Base case: If the number is 0, stop recursion
+    return 1 + countDigits(n / 10);
+}
+
+void p6() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << countDigits(n) << endl;
+}
+
+int sumDigits(int n) {
+    if (n == 0) return 0;
+    return n % 10 + sumDigits(n / 10);
+}
+
+void p7() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << sumDigits(n) << endl;
+}
+
+int power(int n, int k) {
+    if (k == 0) return 1;
+    return n * power(n, k - 1);
+}
+
+void p8() {
+    int n,k;
+    cout << "Enter a number: ";
+    cin >> n;
+    cin >> k;
+
+    cout << power(n, k) << endl;
+}
+
+void reverse(int n) {
+    if (n == 0) return ;
+    cout << n%10;
+    reverse(n / 10);
+}
+
+void p9() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    reverse(n);
+    cout << endl;
+}
+
+
+
+int main() {
+    // p1();
+    // p2();
+    // p3();
+    // p4();
+    // p5();
+    // p6();
+    // p7();
+    // p8();
+    // p9();
+    return 0;
+}
